9d608dac827c339b10301b0b4f19592cc1d208c7
Ibrahimnalmanaseer <ibrahimnalmanaseer@gmail.com>
2022-08-21 19:53:07 +0300

    version 1

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Horned from "./hornedBeast";
 import markhor from './images/markhor.jpg';
-import nubian from './images/nubianibex.jpg'
+import nubian from './images/nubian.jpg'
 
 
 class Main extends React.Component{
